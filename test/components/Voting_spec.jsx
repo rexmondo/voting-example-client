f37425b66c3a8e5b4008ab3fee037d3ae9fea5fa
Ben Hoffman <dbenhoffman@gmail.com>
2015-10-31 18:22:45 -0600

    button now passes vote

--- test/components/Voting_spec.jsx
+++ test/components/Voting_spec.jsx
@@ -17,5 +17,16 @@ describe('Voting', () => {
     expect(buttons[0].textContent).to.equal('Trainspotting');
     expect(buttons[1].textContent).to.equal('28 Days Later');
   });
+
+  it('invokes callback when a button is clicked', () => {
+    let VotedWith;
+    const vote = (entry) => votedWith = entry;
+
+    const component = renderIntoDocument(
+      <Voting pair={["Trainspotting", "28 Days Later"]}
+              vote={vote}
+      />
+    );
+  });
   
 });
