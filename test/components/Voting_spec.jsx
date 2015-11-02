cb29cf02161f3cfacee993f3be788e49413a95e9
Ben Hoffman <dbenhoffman@gmail.com>
2015-11-01 21:25:05 -0700

    more voting tests

--- test/components/Voting_spec.jsx
+++ test/components/Voting_spec.jsx
@@ -1,4 +1,5 @@
 import React from 'react/addons';
+import ReactDOM from 'react-dom';
 import Voting from '../../src/components/Voting';
 import {expect} from 'chai';
 
@@ -43,6 +44,28 @@ describe('Voting', () => {
     expect(buttons.length).to.equal(2);
     expect(buttons[0].hasAttribute('disabled')).to.equal(true);
     expect(buttons[1].hasAttribute('disabled')).to.equal(true);
+  });
+  
+  it('adds label to the voted entry', () => {
+    const component = renderIntoDocument(
+      <Voting pair={["Trainspotting", "28 Days Later"]}
+              hasVoted="Trainspotting" />
+    );
+    const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+
+    expect(buttons[0].textContent).to.contain('Voted');
+  });
+
+  it('renders just the winner when there is one', () => {
+    const component = renderIntoDocument(
+      <Voting winner="Trainspotting" />
+    );
+    const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+    expect(buttons.length).to.equal(0);
+
+    const winner = ReactDOM.findDOMNode(component.refs.winner);
+    expect(winner).to.be.ok;
+    expect(winner.textContent).to.contain('Trainspotting');
   });
   
 });
