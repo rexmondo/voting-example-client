ba9dfac77b0202bc4dc15937712821b7714f072d
Ben Hoffman <ben.hoffman@aware360.com>
2015-11-02 10:20:51 -0700

    finished the UI and UI tests

--- test/components/Results_spec.jsx
+++ test/components/Results_spec.jsx
@@ -1,4 +1,5 @@
 import React from 'react/addons';
+import ReactDOM from 'react-dom';
 import {List, Map} from 'immutable';
 import Results from '../../src/components/Results';
 import {expect} from 'chai';
@@ -22,6 +23,32 @@ describe('Results', () => {
     expect(train).to.contain('5');
     expect(days).to.contain('28 Days Later');
     expect(days).to.contain('0');
+  });
+
+  it('invokes the next callback when next button is clicked', () => {
+    let nextInvoked = false;
+    const next = () => nextInvoked = true;
+
+    const pair = List.of('Trainspotting', '28 Days Later');
+    const component = renderIntoDocument(
+      <Results pair={pair} tally={Map()} next={next} />
+    );
+    Simulate.click(ReactDOM.findDOMNode(component.refs.next));
+    
+    expect(nextInvoked).to.equal(true);
+  });
+
+  it('renders the winner when there is one', () => {
+    const component = renderIntoDocument(
+      <Results 
+        winner="Trainspotting"
+        pair={["Trainspotting", "28 Days Later"]}
+        tally={Map()}
+      />
+    );
+    const winner = ReactDOM.findDOMNode(component.refs.winner);
+    expect(winner).to.be.ok;
+    expect(winner.textContent).to.contain('Trainspotting');
   });
 
 });
